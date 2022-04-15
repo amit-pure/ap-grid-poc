aa85ebb7f7f24084d5f7f59d1cb70e7011f4d042
amit-pure <amitpure@live.in>
2022-04-15 10:46:57 -0500

    added missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SlickGridModule } from './slick-grid/slick-grid.module';
 import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MatTabsModule, SlickGridModule ],
+  imports:      [ BrowserModule, FormsModule,MatTabsModule, SlickGridModule, BrowserAnimationsModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
