a986c60b6afe8b7eb8f35be315781ec02f7640ec
amit-pure <amitpure@live.in>
2022-04-15 10:24:20 -0500

    Added material tabs and chenged styes to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SlickGridModule } from './slick-grid/slick-grid.module';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,MatTabsModule, SlickGridModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
