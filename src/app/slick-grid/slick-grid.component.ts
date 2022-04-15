da9045f0901ceb95409871eaad1fcaa259a6bfdf
amit-pure <amitpure@live.in>
2022-04-15 13:17:08 -0500

    Slick grid changes

--- src/app/slick-grid/slick-grid.component.ts
+++ src/app/slick-grid/slick-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GridOption, Formatters } from 'angular-slickgrid';
+//import { Coulmn  } from './../modules/angular-slickgrid';
 
 @Component({
   selector: 'app-slick-grid',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slick-grid.component.scss']
 })
 export class SlickGridComponent implements OnInit {
-
+  columnDefinitions1: Column[] = [];  
+  gridOptions1!: GridOption;  
+  dataset1!: any[];
   constructor() { }
 
   ngOnInit() {
+    this.columnDefinitions1 = [
+      { id: 'title', name: 'Title', field: 'title', sortable: true },
+      { id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true },
+      { id: '%', name: '% Complete', field: 'percentComplete', sortable: true },
+      { id: 'start', name: 'Start', field: 'start', formatter: Formatters.dateIso },
+      { id: 'finish', name: 'Finish', field: 'finish', formatter: Formatters.dateIso },
+      { id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', sortable: true }
+    ];
+    this.gridOptions1 = {
+      enableAutoResize: false,
+      enableSorting: true,
+      gridHeight: 225,
+      gridWidth: 800,
+    };
+  }
+
+  mockData(count: number) {
+    // mock a dataset
+    const mockDataset = [];
+    for (let i = 0; i < count; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor((Math.random() * 29));
+      const randomPercent = Math.round(Math.random() * 100);
+
+      mockDataset[i] = {
+        id: i,
+        title: 'Task ' + i,
+        duration: Math.round(Math.random() * 100) + '',
+        percentComplete: randomPercent,
+        start: new Date(randomYear, randomMonth + 1, randomDay),
+        finish: new Date(randomYear + 1, randomMonth + 1, randomDay),
+        effortDriven: (i % 5 === 0)
+      };
+    }
+
+    return mockDataset;
   }
 
 }
--- src/app/slick-grid/slick-grid.module.ts
+++ src/app/slick-grid/slick-grid.module.ts
@@ -1,6 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularSlickgridModule } from 'angular-slickgrid';
 @NgModule({
-  imports:  [ ],  
+  imports:  [
+    BrowserModule,
+    FormsModule,
+    AngularSlickgridModule.forRoot({
+      // add any Global Grid Options/Config you might want
+      // to avoid passing the same options over and over in each grids of your App
+     // enableAutoResize: true,
+      // autoResize: {
+      //   container: '#grid-container',
+      //   rightPadding: 10
+      // }
+    })
+  ],  
   exports:[ ]  
 })
 export class SlickGridModule { }
